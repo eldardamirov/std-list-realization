a221a2e8d6501a7cf736c62267069bd22e8aa790
eldardamirov <eldardamirov75@mail.ru>
2018-02-14 23:23:12 +0300

    Initial Commit

---
+++ std list realization/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  std list realization
+//
+//  Created by Эльдар Дамиров on 14.02.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
